15b20dee36c1eb50ce05f9521b40e0509a3624c1
linsir <student_yxl@126.com>
2024-08-03 03:31:10 +0800

    css 提示问题解决

--- apps/auth/src/views/login/index.vue
+++ apps/auth/src/views/login/index.vue
@@ -1,8 +1,10 @@
-<script setup lang="ts">
-    onMounted(()=>{
-        console.log("aaaa");
-    })
-</script>
 <template>
-    login
+  <div class="login-container"></div>
 </template>
+
+<script setup lang="ts">
+// 导入 login.scss 文件
+import "@/styles/login.scss";
+</script>
+
+<style lang="scss" scoped></style>
